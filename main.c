67b09fe7f2f934f1ba2c9cc6fd3039b7d28941ef
John Kindem <461425614@qq.com>
2018-12-03 14:09:28 +0800

    [add] 一些代码

--- main.c
+++ main.c
@@ -12,8 +12,22 @@
 #include <fcntl.h>
 #include <sys/select.h>
 
+// 函数定义
+void showLanHostAndIP();
+int tcpPortScan(char *ip, int port);
+
 int main(int argc, char *argv[]) {
-    
+
 
     return 0;
+}
+
+// 显示局域网主机名和IP
+void showLanHostAndIP() {
+
+}
+
+// tcp 端口扫描
+int tcpPortScan(char *ip, int port) {
+    
 }
