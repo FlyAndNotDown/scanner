2718f7c355f5018579226d73db278130a2397bab
John Kindem <461425614@qq.com>
2018-12-03 15:50:02 +0800

    [add] showLanHostAndIP

--- main.c
+++ main.c
@@ -12,19 +12,82 @@
 #include <fcntl.h>
 #include <sys/select.h>
 
+// 宏定义
+#define MAX_LAN_HOST_NUM 200
+#define BUFFER_SIZE 1024
+#define CHAR_BUFFER_SIZE 40
+
 // 函数定义
 void showLanHostAndIP();
 int tcpPortScan(char *ip, int port);
 
-int main(int argc, char *argv[]) {
+// 定义结构体用于存储主机名和IP
+typedef struct {
+    char name[40];
+    char ip[40];
+} Host;
 
+// 全局变量
+// 用于存储局域网主机名和IP的数组
+Host lanHosts[MAX_LAN_HOST_NUM];
+int lanHostsNum = 0;
+
+int main(int argc, char *argv[]) {
+    // 参数判断
+    if (argc < 2) {
+        printf("Usage:\n");
+        printf("1. .\/scanner -lan\n");
+        printf("2. .\/scanner -ip ipAddress\n");
+        return 0;
+    }
+
+    // 两种用法
+    if (!strcmp(argv[1], "-lan")) {
+        // 获取并显示局域网主机名和IP
+        showLanHostAndIP();
+    } else if (!strcmp(argv[1], "-ip")) {
+        // TODO
+    } else {
+        printf("Unknown params.\n");
+    }
 
     return 0;
 }
 
 // 显示局域网主机名和IP
 void showLanHostAndIP() {
+    // 文件结构体
+    FILE *hostNameFile, *hostIpFile;
+    // 缓冲区
+    char buffer1[CHAR_BUFFER_SIZE], buffer2[CHAR_BUFFER_SIZE];
+    int i;
 
+    // 执行shell指令获取局域网所有主机名和IP，并将结果输出到.host
+    system("arp -a | cut -d \" \" -f 1 > .hname");
+    system("arp -a | cut -d \" \" -f 2 | sed "s\/\(\/\/g" | sed "s\/\)\/\/g" > .hip");
+
+    // 打开文件
+    hostNameFile = fopen(".\/.hname", "r");
+    hostIpFile = fopen(".\/.hip", "r");
+
+    // 不断读取并且存入结构体
+    buffer1 = fgets(buffer, CHAR_BUFFER_SIZE, hostNameFile);
+    buffer2 = fgets(buffer, CHAR_BUFFER_SIZE, hostIpFile);
+    while (buffer1 && buffer2) {
+        strcpy(lanHosts[lanHostsNum], buffer1);
+        strcpy(lanHosts[lanHostsNum++], buffer2);
+    }
+
+    // 关闭文件
+    fclose(hostNameFile);
+    fclose(hostIpFile);
+
+    // 先输出局域网主机信息
+    printf("All host info in LAN: \n");
+    printf("No\t\tName\t\tIP\n");
+    for (i = 0; i < lanHostsNum; i++) {
+        printf("%d\t\t%s\t\t%s\n", i + 1, lanHosts[i].name, lanHosts[i].ip);
+    };
 }
 
 // tcp 端口扫描
